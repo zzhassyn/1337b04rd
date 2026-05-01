a509d69cf7055b4f114a1cf16ce7a0172df5ebc3
Zhassyn <zhasynz05@mail.ru>
2026-05-01 22:35:22 +0500

    feat: add connection with db

---
+++ internal/adapters/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+func Open(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("db.Open: %w", err)
+	}
+	if err := db.PingContext(context.Background()); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("db.Open ping: %w", err)
+	}
+
+	return db, nil
+}
+
+func RingMigrations(db *sql.DB, sql string) error {
+	if _, err := db.Exec(sql); err != nil {
+		return fmt.Errorf("db.RunMigrations: %w", err)
+	}
+
+	return nil
+}

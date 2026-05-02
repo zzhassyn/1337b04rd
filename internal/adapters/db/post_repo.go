d6890dc8d7b17c29acd58cf71900b6f2cf9d070c
Zhassyn <zhasynz05@mail.ru>
2026-05-02 11:31:46 +0500

    fix: GetByID added a new feature that performs a scans data(scanPost and queryPosts) and adds a new method ListActive

--- internal/adapters/db/post_repo.go
+++ internal/adapters/db/post_repo.go
@@ -38,17 +38,64 @@ func (r *PostRepo) GetByID(ctx context.Context, id int64) (*domain.Post, error) 
         FROM posts
         WHERE id = $1`
 
+	row := r.db.QueryRowContext(ctx, query, id)
+
+	post, err := scanPost(row)
+	if err != nil {
+		return nil, fmt.Errorf("PostRepo.GetByID: %w", err)
+	}
+	return post, nil
+}
+
+func (r *PostRepo) ListActive(ctx context.Context) ([]*domain.Post, error) {
+	query := `
+		SELECT id, title, content, image_url, user_name, avatar_url, session_id, status, created_at, last_comment_at
+        FROM posts
+        WHERE status = 'active' ORDER BY created_at DESC`
+
+	return r.queryPosts(ctx, query)
+}
+
+func (r *PostRepo) queryPosts(ctx context.Context, query string) ([]*domain.Post, error) {
+	rows, err := r.db.QueryContext(ctx, query, domain.StatusActive)
+	if err != nil {
+		return nil, fmt.Errorf("PostRepo.queryPosts: %w", err)
+	}
+	defer rows.Close()
+
+	var posts []*domain.Post
+
+	for rows.Next() {
+		post, err := scanPost(rows)
+		if err != nil {
+			return nil, fmt.Errorf("PostRepo.queryPosts scan: %w", err)
+		}
+
+		posts = append(posts, post)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("PostRepo.queryPosts rows: %w", err)
+	}
+
+	return posts, nil
+}
+
+type scanner interface {
+	Scan(dest ...any) error
+}
+
+func scanPost(s scanner) (*domain.Post, error) {
 	var (
 		post                domain.Post
 		imageURL, sessionID sql.NullString
 		lastCommentAt       sql.NullTime
 	)
 
-	err := r.db.QueryRowContext(ctx, query, id).Scan(&post.ID, &post.Title, &post.Content,
+	err := s.Scan(&post.ID, &post.Title, &post.Content,
 		&imageURL, &post.UserName, &post.AvatarURL, &sessionID, &post.Status, &post.CreatedAt, &lastCommentAt)
-
 	if err != nil {
-		return nil, fmt.Errorf("PostRepo.GetByID: %w", err)
+		return nil, err
 	}
 
 	if imageURL.Valid {
